1773c56fddf515458e4aec3766d1e7deebd37255
bodii <1401097251@qq.com>
2026-09-01 15:48:59 +0800

    chore(var): setting wrangler vars

--- src/config/strapi_client.ts
+++ src/config/strapi_client.ts
@@ -1,8 +1,6 @@
 import { strapi } from '@strapi/client';
-import { STRAPI_BASE_URL } from "astro:env/server";
 
-
-const BASE_URL = STRAPI_BASE_URL || "http://localhost:1337";
+const BASE_URL = import.meta.env.STRAPI_BASE_URL || 'http://localhost:1337';
 
 const BASE_API_URL = BASE_URL + "/api";
 const strapiClient = strapi({ baseURL: BASE_API_URL });
---
+++ src/env.d.ts
@@ -0,0 +1,8 @@
+interface ImportMetaEnv {
+    readonly STRAPI_BASE_URL: string;
+}
+
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
