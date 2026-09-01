69ecb5e4611f084d65ab0e3e0fb4f0732a5bfb81
bodii <1401097251@qq.com>
2026-09-01 14:45:42 +0800

    chore(var): Add cloudflare to modify variable settings.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import cloudflare from '@astrojs/cloudflare';
@@ -7,7 +7,12 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
+    plugins: [tailwindcss()],
+  },
+  env: {
+    schema: {
+      STRAPI_BASE_URL: envField.string({ context: "server", access: "public", optional: true }),
+    }
   },
 
   adapter: cloudflare(),
