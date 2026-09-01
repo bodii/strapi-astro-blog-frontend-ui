bcb9bcc06a77bbea9a533925c37b96e8332b41f6
bodii <1401097251@qq.com>
2026-09-01 15:06:13 +0800

    chore(var): cloudflare vars use

--- src/config/strapi_client.ts
+++ src/config/strapi_client.ts
@@ -6,4 +6,5 @@ const BASE_URL = STRAPI_BASE_URL || "http://localhost:1337";
 
 const BASE_API_URL = BASE_URL + "/api";
 const strapiClient = strapi({ baseURL: BASE_API_URL });
+
 export { strapiClient, BASE_URL, BASE_API_URL };
